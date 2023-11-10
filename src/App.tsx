cc5f08caa4397c58fd43ee0ba0e4a5345a7a0ec0
Italo Simões <2d.italo@gmail.com>
2023-11-10 16:23:52 -0300

    sobre component criado e estilizado

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.scss";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import Sobre from "./components/Sobre/Sobre";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Sobre />
     </>
   );
 }
---
+++ src/components/Sobre/Sobre.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.scss";
+
+const Sobre = () => {
+  return (
+    <div id="sobre" className="sobre_wrapper">
+      <h2>Sobre mim</h2>
+      <div className="sobre_imagem_container">
+        <div className="sobre_image_card" />
+      </div>
+      <h3 className="sobre_text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+        exercitationem accusamus officia, ipsa debitis, aspernatur quia
+        voluptatem in saepe nobis dolorum repudiandae alias vero voluptas totam
+        error fuga quaerat aut!
+      </h3>
+    </div>
+  );
+};
+
+export default Sobre;
