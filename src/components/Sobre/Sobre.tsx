79cc1f0116f342426da134809bfca92c91a1b071
Italo Simões <2d.italo@gmail.com>
2023-11-28 14:41:37 -0300

    texto da seção atualizado

--- src/components/Sobre/Sobre.tsx
+++ src/components/Sobre/Sobre.tsx
@@ -8,12 +8,27 @@ const Sobre = () => {
       <div className="sobre_imagem_container">
         <div className="sobre_image_card" />
       </div>
-      <h3 className="sobre_text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-        exercitationem accusamus officia, ipsa debitis, aspernatur quia
-        voluptatem in saepe nobis dolorum repudiandae alias vero voluptas totam
-        error fuga quaerat aut!
-      </h3>
+      <br />
+      <div className="sobre_text">
+        <h3>
+          Natural do estado do Maranhão, graduado pela Universidade Federal do
+          Maranhão (UFMA) no curso de Ciência da Computação, venho atuando no
+          desenvolvimento web Front-end desde 2021.
+        </h3>
+        <h3>
+          Utilizando como principais tecnologias <b>React</b>, <b>Angular</b>,{" "}
+          <b>Javascript</b>, <b>Typescript</b>, além de <b>HTML5</b> e{" "}
+          <b>CSS3</b>, venho desenvolvendo aplicações Web com foco em
+          acessibilidade, praticidade e usabilidade para que todos os usuários
+          possam ser incluídos e consigam utilizar as aplicações de acordo com
+          sua necessidade.
+        </h3>
+        <h3>
+          Busco sempre eficiência e simplicidade na criação de projetos para que
+          sua utilização seja intuitiva, eficaz e prazerosa por parte dos
+          usuários.
+        </h3>
+      </div>
     </div>
   );
 };
